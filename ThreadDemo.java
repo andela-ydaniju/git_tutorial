80a39c56efd40c5fb7e75a3165aaf7c1ac37cf4c
andela-wakinsanmi <waleola.akinsanmi@andela.com>
2015-12-06 13:35:32 +0100

    Add thread demo to folder

---
+++ ThreadDemo.java
@@ -0,0 +1,18 @@
+ class MyThread extends Thread {
+  public void run(){
+    for(int i =0; i<10; i++){
+        System.out.println("Child Thread");
+      }
+  }
+
+}
+
+class ThreadDemo {
+  public static void main(String args[]){
+    MyThread A = new MyThread();
+    A.start();
+    for(int i = 0; i<10; i++){
+      System.out.println("Main Thread");
+    }
+  }
+}
